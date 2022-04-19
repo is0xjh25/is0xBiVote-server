4cf88884f285caad47251a7d299682ca9c785be9
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2022-04-19 11:35:25 +0800

    Update: cors setting

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['FRONTEND_URL'],'http://localhost:3000'
+    origins ENV['HEROKU_FRONTEND_URL'], "https://bivote-606ca.web.app", 'http://localhost:3000'
 
     resource '*',
       headers: :any,
